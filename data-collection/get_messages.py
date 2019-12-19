8eb9e2b964c2a9fa3adfb7ddcd067ccab63f4c83
Pratik Vaidya <psvaidya@umich.edu>
2019-12-18 22:31:42 -0800

    seperate data-collection module

--- data-collection/get_messages.py
+++ data-collection/get_messages.py
@@ -24,13 +24,13 @@ def lambda_handler(event, context):
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        # else:
+        #     flow = InstalledAppFlow.from_client_secrets_file(
+        #         'credentials.json', SCOPES)
+        #     creds = flow.run_local_server(port=0)
+        # # Save the credentials for the next run
+        # with open('token.pickle', 'wb') as token:
+        #     pickle.dump(creds, token)
 
     service = build('gmail', 'v1', credentials=creds)
 
