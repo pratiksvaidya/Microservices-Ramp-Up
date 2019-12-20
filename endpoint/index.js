27bb378868c7156f367951c7b693ef967ec7a901
Pratik Vaidya <psvaidya@umich.edu>
2019-12-20 10:08:50 -0800

    add endpoint reference

--- endpoint/index.js
+++ endpoint/index.js
@@ -1,3 +1,5 @@
+// https://firstclassjs.com/serverless-crud-api-using-aws-lambda-dynamodb-api-gateway-and-node-js/
+
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
