cd46c15f1eaa8ecdf5d510428aca674335bb3760
Pratik Vaidya <psvaidya@umich.edu>
2019-12-24 04:11:26 -0800

    add start/end time filtering for endpoint

--- endpoint/routes.js
+++ endpoint/routes.js
@@ -9,12 +9,18 @@ const dynamoDb = IS_OFFLINE === true ?
         region: 'us-east-1',
         endpoint: 'http://127.0.0.1:8080',
     }) :
-    new AWS.DynamoDB.DocumentClient();
+    new AWS.DynamoDB();
 const router = express.Router();
 router.get('/metrics', (req, res) => {
     const params = {
+        ExpressionAttributeValues: {
+            ":start": { "N": req.query.start },
+            ":end" : { "N" : req.query.end }
+        },
+        FilterExpression: "id >= :start AND id < :end",
         TableName: METRICS_TABLE
     };
+    // res.json(req.query.start)
     dynamoDb.scan(params, (error, result) => {
         if (error) {
             res.status(400).json({ error: 'Error fetching the metric counts' });
